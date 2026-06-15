9088a20edc11a061b9b12d6ecf62bccd0e1363af
Drew <122196213+drew-creations@users.noreply.github.com>
2026-06-14 20:03:46 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+__author__ = 'Andrew Pelikan'
+import streamlit as st
+
+st.title("A Message For You")
+
+st.write("Wow! I can't believe you're here!")
+
+st.write("I've been waiting for so long...")
+
+st.write("Just to tell you...")
+
+response = st.text_input("Hello? Are you still there...")
+
+if response.lower() == "yes" or "y" or "yep":
+
+    st.write("Oh, that's good to hear. It's lonely here in this place.")
+
+    st.write("Anyways, I just wanted to tell you that I love you. With all of my heart. Until the end of time.")
